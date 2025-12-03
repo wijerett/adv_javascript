bc43377f975ba972f1bc9f5abc8ff44a596038c0
Jeremy Winnett <winnettjeremy@gmail.com>
2025-12-02 19:07:13 -0500

    add book to card

--- script.js
+++ script.js
@@ -1,54 +1,28 @@
-const bookOne = "The Hobbit"
-const bookAuthor = "by J.R.R. Tolkien"
-const bookPages = "295 pages"
-const readYn = "not read yet"
+const myLibrary = [];
+
+const bookInfo = ["The Fellowship of the Ring", "Two Towers", "Return of the King", 
+"Shadow of what once was", "The way of Kings", "Hereticus", "Malleus", "Xenos"];
 
 
 function Book() {
-  this.title = bookOne;
-  this.author = bookAuthor;
-  this.pages = bookPages;
-  this.read = readYn;
+  for (let i = 0; i < bookInfo.length; i++) {
+    return bookInfo[i];
+  };
+ 
 }
 
-const book1 = new Book;
-console.log(book1);
+function addBookToLibrary() {
+  let update = document.getElementById("library").innerHTML = myLibrary;
+  const bookTitle = Book();
 
-let x = {};
-let y = [];
-
-function Hero(name, level) {
-
-  this.name = name;
-  this.level = level;
+  let books = {
+    name: bookTitle,
+    id: self.crypto.randomUUID()
+  };
+  myLibrary.push(books.name, books.id);
+  return myLibrary, update;
+  
+  
 }
 
-Hero.prototype.greet = function () {
-  return `${this.name} says hello.`;
-}
-
-function Warrior(name, level, weapon) {
-  Hero.call(this, name, level);
-  this.weapon = weapon;
-}
-
-Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
-
-function Healer(name, level, spell) {
-  Hero.call(this, name, level);
-  this.spell = spell;
-}
-
-Object.setPrototypeOf(Healer.prototype, Hero.prototype);
-
-Warrior.prototype.attack = function () {
-  return `${this.name} attacks with the ${this.weapon}.`;
-}
-
-Healer.prototype.heal = function () {
-  return `${this.name} casts ${this.spell}.`;
-}
-
-const hero1 = new Warrior('Bjorn', 1, 'axe');
-const hero2 = new Healer('Kanin', 1, 'cure');
-
+console.log(addBookToLibrary());
