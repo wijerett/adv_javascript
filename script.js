eb3ad85be6701c517a1bf91dfe96e31e2d07b474
Jeremy Winnett <winnettjeremy@gmail.com>
2026-04-14 05:32:59 -0400

    added form validation

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 var myLibrary = [];
 var library = [];
 
-
-
 let bookInfo = [
   ["The Fellowship of the Ring, JRR, 560 pages, The Book was read"],
   ["Two Towers, JRR, 700 pages, The Book was not read"],
@@ -16,15 +14,14 @@ let bookInfo = [
 
 let currentIndex = 0;
 
-
 class Book {
+
   constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.id = self.crypto.randomUUID();
-
   }
 
   static bookCycle() {
@@ -45,58 +42,48 @@ class Book {
     library.push(book);
     book.tileSet();
   }
-
   static addSecond() {
     const [title, author, pages, read] = bookInfo[1][0].split(', ');
     const book = new Book(title, author, pages, read);
     library.push(book);
     book.tileSet();
   }
-
   static addThird() {
     const [title, author, pages, read] = bookInfo[2][0].split(', ');
     const book = new Book(title, author, pages, read);
     library.push(book);
     book.tileSet();
   }
-
   static addFourth() {
     const [title, author, pages, read] = bookInfo[3][0].split(', ');
     const book = new Book(title, author, pages, read);
     library.push(book);
     book.tileSet();
   }
-
   static addFifth() {
     const [title, author, pages, read] = bookInfo[4][0].split(', ');
     const book = new Book(title, author, pages, read);
     library.push(book);
     book.tileSet();
   }
-
   static addSixth() {
     const [title, author, pages, read] = bookInfo[5][0].split(', ');
     const book = new Book(title, author, pages, read);
     library.push(book);
     book.tileSet();
   }
-
   static addSeventh() {
     const [title, author, pages, read] = bookInfo[6][0].split(', ');
     const book = new Book(title, author, pages, read);
     library.push(book);
     book.tileSet();
   }
-
   static addEighth() {
     const [title, author, pages, read] = bookInfo[7][0].split(', ');
     const book = new Book(title, author, pages, read);
     library.push(book);
     book.tileSet();
   }
-
-
-  
 
   tileSet() {
     const newDiv = document.querySelector(".new-cards");
@@ -117,8 +104,6 @@ class Book {
       content.appendChild(remove);
     };
     updateCardText();
-
-
     changeRead.addEventListener('click', () => {
       this.toggleStatus();
       updateCardText();
@@ -141,49 +126,6 @@ class Book {
 }
 let book = new Book();
 
-  //I need to access book attributes from Book to newCard class
-
-
-
-const showButton = document.getElementById("showDialog");
-const favDialog = document.getElementById("favDialog");
-const outputBox = document.querySelector("output");
-const selectEl = favDialog.querySelector("input");
-const confirmBtn = favDialog.querySelector("#confirmBtn");
-
-const dialogInput1 = document.getElementById('book-title')
-const dialogInput2 = document.getElementById('author');
-const dialogInput3 = document.getElementById('pages');
-
-
-let value4 = "";
-class Radio {
-  constructor(value4) {
-    this.value4 = value4;
-  }
-  setStatus() {
-    if(document.getElementById('read-y').checked) {
-      value4 = "The Book was read"
-    } else if (document.getElementById('read-n').checked) {
-      value4 = "The Book was not read"
-    }
-    document.getElementById('read-y').checked = false;
-    document.getElementById('read-n').checked = false;
-  
-
-    showButton.addEventListener("click", () => {
-      favDialog.showModal();
-    });
-
-    confirmBtn.addEventListener("click", (event) => {
-      event.preventDefault();
-      favDialog.close(selectEl.value);
-    });
-  };
-}
-const readstatus = new Radio();
-readstatus.setStatus();
-
 
 class getID {
   constructor(value1, value2, value3) {
@@ -199,8 +141,8 @@ class getID {
     if (value1 === "" || value2 === "" || value3 === "") {
       return
     };
+
     const book = new Book(value1, value2, value3, value4);
-      
     myLibrary.push(book);
     library.push(book);
     book.tileSet();
@@ -210,18 +152,100 @@ class getID {
     console.log(library);
     console.log(myLibrary);
   };
-  Confirm(value1, value2, value3, value4) {
-    confirmBtn.addEventListener("click", () => {
-      const book = new Book(value1, value2, value3, value4);
+
+  Cancel() {
+    cancel.addEventListener("click", () => {
+      dialogInput1.value = '';
+      dialogInput2.value = '';
+      dialogInput3.value = '';
+      favDialog.close(selectEl.value);
+    });
+  }
+
+};
+
+
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("favDialog");
+const outputBox = document.querySelector("output");
+const selectEl = favDialog.querySelector("input");
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+const dialogInput1 = document.getElementById('book-title')
+const dialogInput2 = document.getElementById('author');
+const dialogInput3 = document.getElementById('pages');
+const cancel = document.getElementById('cancel');
+
+
+let id = new getID();
+
+let value4 = "";
+
+class Radio {
+  constructor(value1, value2, value3, value4) {
+    this.value1 = value1;
+    this.value2 = value2;
+    this.value3 = value3;
+    this.value4 = value4;
+  }
+  setStatus() {
+    if(document.getElementById('read-y').checked) {
+      value4 = "The Book was read"
+    } else if (document.getElementById('read-n').checked) {
+      value4 = "The Book was not read"
+    }
+    document.getElementById('read-y').checked = false;
+    document.getElementById('read-n').checked = false;
+    showButton.addEventListener("click", () => {
+      
+      favDialog.showModal();
+    });
+
+    confirmBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      function validator() {
+        const title = document.getElementById('book-title');
+        const author = document.getElementById('author');
+        const pages = document.getElementById('pages');
+
+        if (!title.checkValidity()) {
+          title.reportValidity();
+          return false;
+        }
+        if (!author.checkValidity()) {
+          author.reportValidity();
+          return false;
+        }
+        if (!pages.checkValidity()) {
+          pages.reportValidity();
+          return false;
+        }
+        return true;
+      };
+      if (!validator()) return;
+      //myLibrary.push(book);
+      //library.push(book);
       id.getValue();
-      readstatus.setStatus();
-      myLibrary.push(book);
-      library.push(book);
+      //Cancel();
+      //const book = new Book(value1, value2, value3, value4);
+      //getValue();
+      //readstatus.setStatus();
+
+      favDialog.close(selectEl.value);
+      
+      dialogInput1.value = '';
+      dialogInput2.value = '';
+      dialogInput3.value = '';
+
     });
   };
 }
+const readstatus = new Radio();
+readstatus.setStatus();
 
-let id = new getID();
-id.getValue();
-id.Confirm();
+
+
+//let id = new getID();
+//id.getValue();
+//id.Confirm();
+//id.Cancel();
 
